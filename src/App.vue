8409a5cecf6e0cb411ed15fdf4f0bb1241d22174
Sagar Makhija <sagar1309@live.com>
2020-04-26 13:17:42 +0530

    adds Slide Ranger component for input range

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,44 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <SlideRanger
+      :value="0"
+      :steps="0"
+      :min="0"
+      :max="100"
+      @onchange="log()"
+    />
   </div>
 </template>
 
 <script>
+import SlideRanger from "./components/SlideRanger";
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    SlideRanger
+  },
+   methods: {
+    log() {
+      console.log("Changed");
+    }
+  }
 }
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+body {
+  background: #2c3782;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: white;
+  margin-top: 1rem;
 }
 </style>
