65f9e632265298285564b0b64c0f593acbf974ce
Sagar Makhija <sagar1309@live.com>
2020-04-27 02:46:57 +0530

    adds a new component for RangeSlider

---
+++ src/components/RangeSlider.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="range-slider">
+    <input
+      type="range"
+      :value="value"
+      @input="$emit('input', $event.target.value)"
+			:min="min"
+			:max="max"
+			:step="steps"
+      class="pa-1"
+    />
+  </div>
+</template>
+<script>
+export default {
+  name: "RangeSlider",
+  data() {
+    return {
+      value: 0,
+      minValue: 0,
+      maxValue: 100,
+      stepsValue: 1
+    };
+	},
+	props: ['min', 'max', 'steps', 'on-change']
+};
+</script>
+
+<style>
+.range-slider {
+  width: 20%;
+}
+
+.pa-1 {
+  padding: 1rem 0;
+}
+
+/* slider bar */
+input[type="range"] {
+  background: transparent;
+  -webkit-appearance: none;
+  width: 100%;
+  transition: 0.4s all ease-out;
+}
+
+input[type="range"]:focus {
+  outline: none; /* Removes the blue border. You should probably do some kind of focus styling for accessibility reasons though. */
+}
+/* slider thumb */
+input[type="range"]::-webkit-slider-thumb {
+  -webkit-appearance: none;
+
+  border: 1px solid #000000;
+  height: 15px;
+  width: 15px;
+  border-radius: 25%;
+  background: #1208ad;
+  cursor: pointer;
+  margin-top: -4px;
+  box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+  transition: 0.4s all ease-out;
+}
+
+input[type="range"]::-webkit-slider-runnable-track {
+  width: 100%;
+  height: 8.4px;
+  cursor: pointer;
+  box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+  background: #06b49d;
+  border-radius: 1.3px;
+  border: 0.2px solid #010101;
+}
+</style>
--- src/components/SliderWrapper.vue
+++ src/components/SliderWrapper.vue
@@ -4,21 +4,13 @@
       <div class="title">Range Slider</div>
       <div class="body">
         <pre class="mr-1">{{ min }}</pre>
-        <input
-          type="range"
-          v-model="value"
-          :min="min"
-          :max="max"
-          :step="steps"
-          @change="$emit('onchange', 1)"
-          class="pa-1"
-        />
+        <RangeSlider v-model="value" :min="min" :max="max" :steps="steps" @on-change="log" />
         <pre class="ml-1">{{ max }}</pre>
       </div>
       <pre>Value: {{ value }}</pre>
     </div>
 
-    <div class="body">
+    <div class="wrapper-body">
       <div class="slide-range-wrapper-form">
         <div class="form-content px-1">
           <div class="pb-1">Value:</div>
@@ -53,8 +45,12 @@
 </template>
 
 <script>
+import RangeSlider from "./RangeSlider";
 export default {
   name: "SlideRangeWrapper",
+  components: {
+    RangeSlider
+  },
   data() {
     return {
       min: 0,
@@ -92,22 +88,33 @@ export default {
 </script>
 
 <style>
-.slide-range-wrapper .body {
+.slide-range-wrapper .slide-ranger .title {
+  font-size: 2rem;
+  padding: 1rem 0;
+}
+
+.slide-range-wrapper .slide-ranger .body {
   display: flex;
   justify-content: center;
   align-items: center;
 }
-.slide-range-wrapper .slide-range-wrapper-form {
+
+.slide-range-wrapper .wrapper-body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.slide-range-wrapper .wrapper-body .slide-range-wrapper-form {
   width: 25%;
   text-align: left;
   margin-right: 1rem;
 }
 
-.slide-range-wrapper .code-block .title {
+.slide-range-wrapper .wrapper-body .code-block .title {
 	font-size: 1.2rem;
 	font-style: italic;
 }
-.slide-range-wrapper .code-block .code {
+.slide-range-wrapper .wrapper-body .code-block .code {
   background: teal;
   border-radius: 5px;
   border: solid 1px #ccc;
@@ -147,55 +154,5 @@ input[type="text"] {
 }
 .ml-1 {
   margin-left: 1rem;
-}
-.pa-1 {
-  padding: 1rem 0;
-}
-
-.slide-ranger .title {
-  font-size: 2rem;
-  padding: 1rem 0;
-}
-
-.slide-ranger .body {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-/* slider bar */
-input[type="range"] {
-  background: transparent;
-  -webkit-appearance: none;
-  width: 20%;
-  transition: 0.4s all ease-out;
-}
-
-input[type="range"]:focus {
-  outline: none; /* Removes the blue border. You should probably do some kind of focus styling for accessibility reasons though. */
-}
-/* slider thumb */
-input[type="range"]::-webkit-slider-thumb {
-  -webkit-appearance: none;
-
-  border: 1px solid #000000;
-  height: 15px;
-  width: 15px;
-  border-radius: 25%;
-  background: #1208ad;
-  cursor: pointer;
-  margin-top: -4px;
-  box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
-  transition: 0.4s all ease-out;
-}
-
-input[type="range"]::-webkit-slider-runnable-track {
-  width: 100%;
-  height: 8.4px;
-  cursor: pointer;
-  box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
-  background: #06b49d;
-  border-radius: 1.3px;
-  border: 0.2px solid #010101;
 }
 </style>
